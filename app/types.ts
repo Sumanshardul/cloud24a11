21d76d078379899757bab9a3403fe0e3145d1b12
Sumanshardul <sumanshardul55@gmail.com>
2026-05-05 09:06:03 +0545

    chore: initial commit

---
+++ app/types.ts
@@ -0,0 +1,11 @@
+export type QueryItem = {
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  query: string;
+};
+
+export const BACKEND_API_URL =
+  "https://shardul-2024a-etatatcvaaedbjgz.uaenorth-01.azurewebsites.net/queries";
+
+export const APP_API_URL = "/api/queries";
